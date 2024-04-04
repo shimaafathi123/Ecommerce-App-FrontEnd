00754a4d21dc542c094a17eab51c2f5678ed7151
fatmaelhot <fatmaelhot289@gmail.com>
2024-04-05 01:05:12 +0200

    intial structure & add bootstrap

---
+++ src/Components/cart/cart.jsx
@@ -0,0 +1,29 @@
+import react, { useState } from 'react';
+
+const ShoppingCart = () => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
+
+  const removeFromCart = (index) => {
+    const newCart = [...cart];
+    newCart.splice(index, 1);
+    setCart(newCart);
+  };
+
+  const calculateTotal = () => {
+    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+  };
+
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      
+      <p>Total</p>
+    </div>
+  );
+};
+
+export default ShoppingCart;
