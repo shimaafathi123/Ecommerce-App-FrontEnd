0a33ce0096efaefc4673fda37498f5a1db7e7b15
EngAlaaKamal <alaa.kamal.ghonem@gmail.com>
2024-04-07 14:46:54 +0200

    solve user routes

--- src/Components/router/router.jsx
+++ src/Components/router/router.jsx
@@ -2,6 +2,10 @@ import { Outlet, createBrowserRouter } from "react-router-dom"
 import ShoppingCart from "../cart/cart"
 import PageNotFound from "../PageNotFound/PageNotFound"
 import Home  from "../Home/Home";
+
+import Register from "../user/Register"
+import Login from "../user/Login"
+import Profile from "../user/Profile"
 function Layout() {
     return (
       <>
@@ -20,6 +24,19 @@ function Layout() {
         {
           path: "/cart",
           element: <ShoppingCart />,
+        },
+        {
+          path:"/register",
+          element: <Register/>         
+        },
+
+        {
+          path:"/login",
+          element: <Login/>         
+        },
+        {
+          path:"/profile",
+          element: <Profile/>         
         },
         {
           path: "*",
