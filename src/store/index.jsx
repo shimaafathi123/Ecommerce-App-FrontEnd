517a21f04e8ed547b6054aeed7f8847c8af25b4a
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-09 10:29:46 +0200

    change

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -4,8 +4,8 @@ import cartReducer from './cartSlice'
 export default configureStore({
   reducer: {
     cart: cartReducer,
-    wishlist : wishlistReducer,
-    orders: orderReducer,
+    // wishlist : wishlistReducer,
+    // orders: orderReducer,
     // Add other reducers if needed
   },
 });
