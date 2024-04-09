c0ba212286e01320072071ea784903ee3f3866b9
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-09 13:05:48 +0200

    lassttt

--- src/Components/WishList/WishList.jsx
+++ src/Components/WishList/WishList.jsx
@@ -1,165 +1,63 @@
-// import { useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import interceptorInstance from "../../axios/axios";
-// import { Button } from "react-bootstrap";
-// import { Link, useNavigate } from "react-router-dom";
-// import { setWishlist } from "../../store/wishlistSlice";
-// import { setCart } from "../../store/cartSlice";
-// import Navbar from "../Navbar/Navbar";
-// import { ToastContainer, toast } from "react-toastify";
-// //import Footer from "../Footer/Footer"; 
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchWishlist, removeFromWishlist } from "../../store/wishlistSlice";
 
-// function WishList() {
-//   const wishlist = useSelector((state) => state.wishlist.wishlist);
-//   const cart = useSelector((state) => state.cart.cart);
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
+function WishlistPage() {
+  const dispatch = useDispatch();
+  const wishlist = useSelector((state) => state.wishlist.list);
+  const isLoading = useSelector((state) => state.wishlist.isLoading);
+  const error = useSelector((state) => state.wishlist.error);
 
-//   useEffect(() => {
-//     interceptorInstance
-//       .get("users/wishlist")
-//       .then((response) => {
-//         console.log(response);
-//         dispatch(setWishlist(response.data[0].favs));
-//       })
-//       .catch((error) => console.log(error));
-//   }, []);
+  useEffect(() => {
+    dispatch(fetchWishlist());
+  }, [dispatch]);
 
-//   const removeItem = (item) => {
-//     interceptorInstance
-//       .delete(`users/wishlist/items/${item.product.id}`)
-//       .then((response) => {
-//         console.log(response)
-//         dispatch(
-//           setWishlist(
-//             wishlist.filter((wishlist_item) => wishlist_item.id !== item.id)
-//           )
-//         );
-//       })
-//       .catch((error) => console.log(error));
-//     console.log(wishlist);
-//   };
+  const handleRemoveFromWishlist = async (productId) => {
+    await dispatch(removeFromWishlist(productId));
+    dispatch(fetchWishlist());
+  };
 
-//   const addToCartHandler = (item) => {
-//     const existed = cart.findIndex(
-//       (cartItem) => cartItem.product.id === item.product.id
-//     );
-//     if (existed === -1) {
-//       interceptorInstance
-//         .post(`users/cart/items/${item.product.id}/add`, { quantity: 1 })
-//         .then((response) => dispatch(setCart([...cart, response.data])))
-//         .catch((error) => {
-//           console.error(error);
-//           if (error.response && error.response.status >= 400) {
-//             toast.error(error.response.data.error[0] || "Wrong Credintials", {
-//               position: "top-center",
-//             });
-//           } else if (error.request) {
-//             console.log(error.request);
-//           } else {
-//             console.log("Error", error.message);
-//           }
-//           console.log(error.config);
-//         });
-//     } else {
-//       navigate("/cart");
-//     }
-//   };
+  return (
+    <div>
+      <h1>Wishlist</h1>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {!isLoading && !error && wishlist?.length === 0 && (
+        <p>Your wishlist is empty</p>
+      )}
+      {!isLoading && !error && wishlist && (
+        <div className="container">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 gap-4 align-items-center mt-4">
+            {wishlist?.map((item) => (
+              <div key={item.id} className="col">
+                <div className="card">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="card-img-top"
+                    style={{ height: "40vh", objectFit: "cover" }}
+                  />
+                  <div className="card-body">
+                    <h3 className="card-title font-weight-bold">{item.name}</h3>
+                    <p className="card-text text-muted mb-2">
+                      Description: {item.description}
+                    </p>
+                    <p className="card-text text-muted mb-2">Price: {item.price}</p>
+                    <button
+                      onClick={() => handleRemoveFromWishlist(item.id)}
+                      className="btn btn-primary w-100 mt-2"
+                    >
+                      Remove from Wishlist
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
 
-//   return (
-//     <>
-//       <Navbar />
-//       <ToastContainer />
-//       <div className="container mx-auto mt-10">
-//         <div className="card md:shadow-md justify-center px-2 "> 
-//           <div className="d-flex justify-content-between border-bottom pb-8 px-5 "> 
-//             <h1 className="font-weight-bold text-2xl">Wish List</h1> 
-//             <h2 className="font-weight-bold text-2xl text-uppercase">{wishlist.length} {wishlist.length > 1 ? "Items" : "Item"}</h2> 
-//           </div>
-//           {wishlist.length ? (
-//             <>
-//               <div className="d-none lg:block"> 
-//                 <table className="w-11/12">
-//                   <tbody className="py-6">
-//                     {wishlist.map((item) => (
-//                       <tr key={item.id} className="my-10 text-center py-5">
-//                         <td className="d-flex align-items-center"> 
-//                           <div className="p-5">
-//                             <img className="h-40" src={item.product.image} alt={item.product.name} />
-//                           </div>
-//                           <div className="d-flex flex-column justify-content-between flex-grow"> 
-//                             <span className="font-weight-bold text-lg">{item.product.name}</span>
-//                             <span className="text-danger text-m">fashmart</span>
-//                           </div>
-//                         </td>
-//                         <td>
-//                           <div className="d-flex flex-column justify-content-between ml-4">
-//                             {cart.find((selected_item) => selected_item.product.id === item.product.id) ? (
-//                               <Button variant="success" className="shadow-sm bg-success text-white mx-2 my-2 md:my-1 w-2/3" onClick={() => navigate("/cart")}>
-//                                 See purchase options &nbsp; &nbsp; <i className="fa-solid fa-cart-plus text-white mx-2 my-2 md:my-1"></i>
-//                               </Button>
-//                             ) : (
-//                               <Button variant="success" className="shadow-sm bg-success text-white mx-2 my-2 md:my-1" onClick={() => addToCartHandler(item)}>
-//                                 Add to Cart &nbsp; &nbsp; <i className="fa-solid fa-cart-plus text-white"></i>
-//                               </Button>
-//                             )}
-//                           </div>
-//                         </td>
-//                         <td>
-//                           <div className="d-flex flex-column justify-content-between ml-4">
-//                             <Button variant="danger" className="bg-danger text-white" onClick={() => removeItem(item)}>
-//                               Remove &nbsp; &nbsp; <i className="fa-solid fa-x text-white"></i>
-//                             </Button>
-//                           </div>
-//                         </td>
-//                       </tr>
-//                     ))}
-//                   </tbody>
-//                 </table>
-//               </div>
-//               <div className="grid grid-cols-1 gap-4 d-md-none mx-auto"> 
-//                 {wishlist.map((item) => (
-//                   <div className="bg-white shadow-md rounded-lg hover:bg-gray-200 border-gray-400 my-2" key={`${item.id}+${item.product.id}`}>
-//                     <div className="d-flex xs:flex-column md:flex-row justify-content-start py-10 text-lg"> 
-//                       <img className="h-40" src={item.product.image} alt={item.product.name} />
-//                       <div className="d-flex flex-column items-center space-y-5 mx-auto"> 
-//                         <span className="font-weight-bold">{item.product.name}</span> 
-//                         <span className="text-danger">fashmart</span> 
-//                         <div className="d-flex flex-column md:flex-row">
-//                           {cart.find((selected_item) => selected_item.product.id === item.product.id) ? (
-//                             <Button variant="success" className="shadow-sm bg-success text-white mx-2 my-2 md:my-1" onClick={() => navigate("/cart")}>
-//                               See purchase options &nbsp; &nbsp; <i className="fa-solid fa-cart-plus text-white"></i>
-//                             </Button>
-//                           ) : (
-//                             <Button variant="success" className="shadow-sm bg-success text-white mx-2 my-2 md:my-1" onClick={() => addToCartHandler(item)}>
-//                               Add to Cart &nbsp; &nbsp; <i className="fa-solid fa-cart-plus text-white"></i>
-//                             </Button>
-//                           )}
-//                           <Button variant="danger" className="bg-danger text-white" onClick={() => removeItem(item)}>
-//                             Remove &nbsp; &nbsp; <i className="fa-solid fa-x text-white"></i>
-//                           </Button>
-//                         </div>
-//                       </div>
-//                     </div>
-//                   </div>
-//                 ))}
-//               </div>
-//             </>
-//           ) : (
-//             <div className="bg-white items-center p-10"> 
-//               <p className="text-center text-xl">No Thing In your wish List</p> 
-//             </div>
-//           )}
-//         </div>
-//         <div>
-//           <Link to="/" className="d-flex font-weight-bold text-indigo-600 text-lg mt-10"> 
-//             <i className="fa-solid fa-arrow-left text-indigo-600 text-l pr-1 text-lg"></i>
-//             Continue Shopping
-//           </Link>
-//         </div>
-//       </div>
-//       {/* <Footer />  */}
-//     </>
-//   );
-// }
-// export default WishList;
+export default WishlistPage;
