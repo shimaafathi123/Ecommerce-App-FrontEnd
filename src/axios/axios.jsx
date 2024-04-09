be1dab84ca2f9b9adaef5de739bb77cba9110170
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-09 10:26:52 +0200

    change

--- src/axios/axios.jsx
+++ src/axios/axios.jsx
@@ -1,21 +1,21 @@
-import axios from "axios";
-const interceptorInstance = axios.create();
+// import axios from "axios";
+// const interceptorInstance = axios.create();
 
-interceptorInstance.interceptors.request.use(
-  (config) => {
-    config.baseURL ='http://127.0.0.1:8000/';
-    config.withCredentials = true;
-    const user = JSON.parse(sessionStorage.getItem("authUser"));
-    if (user?.token) {
-      config.headers.Authorization = `token ${user.token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// interceptorInstance.interceptors.request.use(
+//   (config) => {
+//     config.baseURL ='http://127.0.0.1:8000/';
+//     config.withCredentials = true;
+//     const user = JSON.parse(sessionStorage.getItem("authUser"));
+//     if (user?.token) {
+//       config.headers.Authorization = `token ${user.token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 
-export default interceptorInstance;
+// export default interceptorInstance;
 
