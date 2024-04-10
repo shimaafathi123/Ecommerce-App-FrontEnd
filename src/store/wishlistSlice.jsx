5178205ee2f1594abd50a705b2d252afdfd1ef3a
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-10 19:09:03 +0200

    Update wishlistSlice.jsx

--- src/store/wishlistSlice.jsx
+++ src/store/wishlistSlice.jsx
@@ -1,90 +1,26 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice } from "@reduxjs/toolkit";
 
-
-
-export const fetchWishlist = createAsyncThunk(
-  "wishlist/fetchWishlist",
-  async () => {
-    const response = await axios.get(`http://localhost:8000/users/wishlist`);
-    console.log(response.data);
-    return response.data;
-  }
-);
-
-export const addToWishlist = createAsyncThunk(
-  "wishlist/addToWishlist",
-  async (productId) => {
-    const response = await axios.post(`http://localhost:8000/users/wishlist/add/`, {
-      product_id: productId,
-    });
-    return response.data;
-  }
-);
-
-export const removeFromWishlist = createAsyncThunk(
-  "wishlist/removeFromWishlist",
-  async (productId) => {
-    try {
-      await axios.delete(`http://localhost:8000/users/wishlist/${productId}`);
-      return productId; // Return the productId to identify which item was removed
-    } catch (error) {
-      console.error("Error occurred while removing item from wishlist:", error);
-      throw error; 
-    }
-  }
-);
-
-const wishlistSlice = createSlice({
+export const wishlistSlice = createSlice({
   name: "wishlist",
   initialState: {
-    list: [],
-    isLoading: false,
-    error: null,
+    wishlist: [],
+    totalAmount: 0,
   },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchWishlist.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(fetchWishlist.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.list = action.payload;
-      })
-      .addCase(fetchWishlist.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message;
-      })
-      .addCase(addToWishlist.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(addToWishlist.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.list = action.payload;
-      })
-      .addCase(addToWishlist.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message;
-      })
-      .addCase(removeFromWishlist.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(removeFromWishlist.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.list = action.payload;
-      })
-      .addCase(removeFromWishlist.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message;
-      });
+  reducers: {
+    setWishlist(state, action) {
+      state.wishlist = action.payload;
+      state.totalAmount = state.wishlist.length;
+      console.log(state.wishlist.length);
+    },
+
+    addToWishlist(state, action) {
+      const item = action.payload;
+      state.wishlist.push(item);
+      state.totalAmount++;
+    },
   },
 });
 
-export const wishlistActions = {
-  fetchWishlist,
-  addToWishlist,
-  removeFromWishlist,
-};
-
+export const { addToWishlist, setWishlist, getWishlist } =
+  wishlistSlice.actions;
 export default wishlistSlice.reducer;
